59b4ec183ce2afd8136bcdc833b2c8f1f518429a
Kemp Atkinson <kempatkinson@gmail.com>
2019-10-10 22:44:40 -0500

    post working, get is not

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var friends = require("../data/friends.js")
+module.exports = function (app) {
+    
+    app.post("/api/friends", function (req, res) {
+       // post survey results in database (array in friends.js)
+       
+        console.log("this is the post" + req.body)
+        friends.push(req.body)
+        res.json(req.body)
+       // for loop to ad   d each question to scores array
+
+       // add MakeFriends to friends.js
+    });
+
+    app.get("/api/friends", function (req, res) {
+        console.log("this is the get" + res.body);
+        res.json(friends)
+    });
+    
+
+  
+};
+
+
+function MakeFriends(name, photo, scores) {
+    this.name = name;
+    this.photo = photo;
+    this.scores = scores;
+}
--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// require("./routes/apiRoutes")(app);
-
-// var htmlRoutes = require("./routes/htmlRouting");
-// htmlRoutes(app);
-
-require("./app/routing/htmlRouting")(app);
-require("./app/routing/apiRouting")(app);
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
