6695b4bcc72b6469b8b3f74f5a2d6edd4a92e8bc
Kemp Atkinson <kempatkinson@gmail.com>
2019-10-13 17:02:08 -0500

    modal working

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,6 +2,6 @@
 var friends = [{
     uName: "Test1",
     uPhone: 45678798,
-    scores: []
+    scores: [ 1, 2 ] 
 }]
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,16 +5,14 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
        // post survey results in database (array in friends.js)
        
-        console.log("this is the post" + req.body)
+        // console.log("this is the post" + req.body)
         friendsdata.push(req.body)
         res.json(req.body)
-       // for loop to ad   d each question to scores array
 
-       // add MakeFriends to friends.js
     });
 
     app.get("/api/friends", function (req, res) {
-        console.log("this is the get" + JSON.stringify(friendsdata, null, 2));
+        // console.log("this is the get" + JSON.stringify(friendsdata, null, 2));
         res.json(friendsdata)
     });
     
@@ -23,8 +21,3 @@ module.exports = function (app) {
 };
 
 
-function MakeFriends(name, photo, scores) {
-    this.name = name;
-    this.photo = photo;
-    this.scores = scores;
-}
